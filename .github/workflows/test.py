002c264289c001ee590aec3c98f611da34796a53
Irina Placinta <irina.placinta@databricks.com>
2024-01-15 14:33:25 +0000

    Changes from context manager

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,5 +1,5 @@
 dbutils.fs.ls("abfss://external-location@testucirina.dfs.core.windows.net/shared/tables")
-dbutils.fs.ls("/mnt/shared-tables")
+dbutils.fs.ls(get_uc_mount_target('/mnt/shared-tables', normalize=True) + "")
 
 
 Hello dbfs:/mnt/shared-tables
