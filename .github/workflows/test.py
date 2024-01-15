2c0903196f9caada2fdc5fc735165c50b8f26f06
irinaplacinta-db <141246242+irinaplacinta-db@users.noreply.github.com>
2024-01-15 15:46:00 +0000

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -2,4 +2,6 @@ dbutils.fs.ls("abfss://external-location@testucirina.dfs.core.windows.net/shared
 dbutils.fs.ls(get_uc_mount_target('/mnt/shared-tables', normalize=True) + "")
 
 
-Hello dbfs:/mnt/shared-tables
+Hello "dbfs:/mnt/shared-tables"
+
+Hello "/mnt/shared-tables/"
